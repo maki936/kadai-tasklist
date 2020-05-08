a9bda784e63b5ef409530bd94abcb66fdc83a929
maki.sato <mkuro4601@gmail.com>
2020-05-08 04:29:39 +0000

    CRUD tasks

---
+++ app/Http/Controllers/TasksController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Task;    // 追加
+
+class TasksController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+     // getでmessages/にアクセスされた場合の「一覧表示処理」
+    public function index()
+    {
+        $messages = Task::all();
+
+        return view('tasks.index', [
+            'messages' => $messages,
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $message = new Task;
+
+        return view('tasks.create', [
+            'message' => $message,
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $message = new Task;
+        $message->content = $request->content;
+        $message->save();
+
+        return redirect('/');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $message = Task::find($id);
+
+        return view('tasks.show', [
+            'message' => $message,
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $message = Task::find($id);
+
+        return view('tasks.edit', [
+            'message' => $message,
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $message = Task::find($id);
+        $message->content = $request->content;
+        $message->save();
+
+        return redirect('/');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+     
+    public function destroy($id)
+    {
+        $message = Task::find($id);
+        $message->delete();
+
+        return redirect('/');
+    }
+}
